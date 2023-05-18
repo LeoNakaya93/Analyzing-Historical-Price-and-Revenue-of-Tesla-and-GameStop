832b283f0ce24b2ca470f09c72c6b3cc82c6d125
LeoNakaya93 <126311741+LeoNakaya93@users.noreply.github.com>
2023-05-17 23:50:31 -0400

    Create code

---
+++ code.py
@@ -0,0 +1,11 @@
+!pip install yfinance==0.1.67
+!mamba install bs4==4.10.0 -y
+!pip install nbformat==4.2.0
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
